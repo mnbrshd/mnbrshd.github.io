5c9ed27e68a49c37567bf4bda666bcb03304ce8a
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2023-10-21 15:42:36 +0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -33,20 +33,14 @@ const config = {
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
+    'Python',
     'JavaScript',
     'React.js',
-    'Node.js',
-    'Nest.js',
     'MySQL',
     'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'CSS'
   ],
   experiences: [
     {
