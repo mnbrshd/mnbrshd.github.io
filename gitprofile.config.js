628521987571f55071ecc91a5a780efbb0048014
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2023-10-21 15:39:43 +0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -173,7 +173,7 @@ const config = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/mnbrshd/gitprofile"
+      class="text-primary" href="https://github.com/mnbrshd/mnbrshd.githun.io"
       target="_blank"
       rel="noreferrer"
     >GitProfile</a> and ❤️`,
